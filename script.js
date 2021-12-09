df34629b9de84e9ef388c571523219a3b118cfd4
emagioli <enzomagioli@gmail.com>
2021-12-09 11:42:32 -0300

    pokemon  theme switch and code adaptation

--- script.js
+++ script.js
@@ -1,57 +1,57 @@
 const FRONT = "card_front";
 const BACK = "card_back";
 
-var techs = [
-    "bootstrap",
-    "css",
-    "electron",
-    "firebase",
-    "html",
-    "javascript",
-    "jquery",
-    "mongo",
-    "node",
-    "react"
+var pokemons = [
+    "abra",
+    "bulbasaur",
+    "charmander",
+    "ditto",
+    "gengar",
+    "mew",
+    "mewtwo",
+    "pikachu",
+    "snorlax",
+    "squirtle"
 ];
 
-createCards(techs);
+createCards(pokemons);
 
-function createCards(techs) {
+function createCards(pokemons) {
     let cards = [];
 
-    for (let tech of techs) {
-        cards.push(createPair(tech));
+    for (let pokemon of pokemons) {
+        cards.push(createPair(pokemon));
     }
 
     console.log(cards.flatMap(pair => pair));
 }
 
-function createPair(tech) {
+function createPair(pokemon) {
     return [{
-        id: generateId(tech),
-        icon: tech,
+        id: generateId(pokemon),
+        icon: pokemon,
         flipped: false
     }, {
-        id: generateId(tech),
-        icon: tech,
+        id: generateId(pokemon),
+        icon: pokemon,
         flipped: false
     }];
 }
 
-function generateId(tech) {
-    return tech + parseInt(Math.random() * 1000);
+function generateId(pokemon) {
+    return pokemon + parseInt(Math.random() * 1000);
 }
 
 
-// function flipCard(card) {
-//     if (!isFlipped(card)) {
-//         card.classList.add("flip");
-//     } else {
-//         card.classList.remove("flip");
-//     }
+function flipCard(card) {
+    if (!isFlipped(card)) {
+        card.classList.add("flip");
+    } else {
+        card.classList.remove("flip");
+    }
 
-// }
+}
 
-// function isFlipped(card) {
-//     return card.classList.contains("flip");
-// }
+function isFlipped(card) {
+    return card.classList.contains("flip");
+}
